39fcbb3bdb6a2ac4ab0386caf5d35f448d794bcc
Richard <richard.yang.tong@gmail.com>
2021-08-01 14:37:25 -0400

    Added notes.

--- s1e6/main.c
+++ s1e6/main.c
@@ -22,5 +22,11 @@ int main()
     printf("int = %d\n", sizeof(int));
     printf("long int = %d\n", sizeof(long));
     printf("long long int = %d\n", sizeof(long long));
+
+    printf("char = %d\n", sizeof(char));
+    printf("_Bool = %d\n", sizeof(_Bool));
+    printf("float = %d\n", sizeof(float));
+    printf("double = %d\n", sizeof(double));
+    printf("long double = %d\n", sizeof(long double));
     return 0;
 }

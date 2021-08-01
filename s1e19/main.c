bf32a797cea9b5dbbed73e8b33a38b1135959863
Richard <richard.yang.tong@gmail.com>
2021-08-01 17:18:41 -0400

    Added notes.

--- s1e19/main.c
+++ s1e19/main.c
@@ -4,16 +4,13 @@
 
 int main()
 {
-    char str1[] = "Original string";
-    char str2[] = "New string";
-    char str3[100];
+    char str1[] = "To be or not to be.";
+    char str2[40];
 
-    strcpy(str1, str2);
-    strcpy(str3, "Copy successful!");
+    strncpy(str2, str1, 5);
+    str2[5] = '\0';
 
-    printf("str1: %s\n", str1);
     printf("str2: %s\n", str2);
-    printf("str3: %s\n", str3);
 
     return 0;
 }

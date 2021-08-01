65c13bf9e6af90503eedc736aa960c1f0886e87f
Richard <richard.yang.tong@gmail.com>
2021-08-01 15:41:58 -0400

    Added notes.

--- s1e9/main.c
+++ s1e9/main.c
@@ -4,15 +4,8 @@
 
 int main()
 {
-    int i, j, k;
-
-    i = 1 + 2;
-    j = 1 + 2 * 3;
-    k = i + j + -1 + pow(2, 3);
-
-    printf("i = %d\n", i);
-    printf("j = %d\n", j);
-    printf("k = %d\n", k);
+    printf("int output : %d\n", 1 + 2.0);
+    printf("float output : %f\n", 1 + 2.0);
 
     return 0;
 }

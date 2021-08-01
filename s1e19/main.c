0045842104d70c317868e55c75ef61479a5cca35
Richard <richard.yang.tong@gmail.com>
2021-08-01 17:04:10 -0400

    Added notes.

---
+++ s1e19/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char str[] = "I love FishC.com!";
+
+    printf("sizeof str = %d\n", sizeof(str));
+    printf("strlen str = %u\n", strlen(str));
+
+    return 0;
+}

ace1315c6a328f15d905b3bd980287e83b68553e
Richard <richard.yang.tong@gmail.com>
2021-08-01 14:17:45 -0400

    Added notes.

---
+++ s1e5/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define URL "http://www.fichc.com"
+#define NAME "YuC Studio"
+#define BOSS "Xiao JiaYu"
+#define YEAR 2010
+#define MONTH 5
+#define DAY 20
+
+int main()
+{
+    printf("%s established at year%d, month%d, day%d\n", NAME, YEAR, MONTH, DAY);
+    return 0;
+}

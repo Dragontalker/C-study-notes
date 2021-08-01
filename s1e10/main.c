e9f1feb62fa929a4ff22c42602d4facf712334ed
Richard <richard.yang.tong@gmail.com>
2021-08-01 16:09:17 -0400

    Added notes.

--- s1e10/main.c
+++ s1e10/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a = 5, b = 3;
+
+    printf("%d\n", 3 > 1 && 1 < 2);
+    printf("%d\n", 3 + 1 || 2 == 0);
+    printf("%d\n", !(a + b));
+    printf("%d\n", !0 + 1 < 1 || !(3 + 4));
+    printf("%d\n", 'a' - 'b' && 'c');
+
     return 0;
 }

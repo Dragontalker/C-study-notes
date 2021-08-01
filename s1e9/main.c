6d3325c0f14ac39ff0ba97edb5fe8bc778015085
Richard <richard.yang.tong@gmail.com>
2021-08-01 16:00:29 -0400

    Added notes.

--- s1e9/main.c
+++ s1e9/main.c
@@ -4,8 +4,13 @@
 
 int main()
 {
-    printf("int output : %d\n", 1 + 2.0);
-    printf("float output : %f\n", 1 + 2.0);
+    int a = 5, b = 3;
+
+    printf("%d\n", 1 < 2);
+    printf("%d\n", a > b);
+    printf("%d\n", a <= b + 1);
+    printf("%d\n", 'a' + 'b' <= 'c');
+    printf("%d\n", (a = 3) > (b = 5));
 
     return 0;
 }

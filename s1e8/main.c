7ce34cff497b3a279bc8449ab4101fe563ba5617
Richard <richard.yang.tong@gmail.com>
2021-08-01 15:12:00 -0400

    Added notes.

--- s1e8/main.c
+++ s1e8/main.c
@@ -3,9 +3,9 @@
 
 int main()
 {
-    char a = 'C';
+    char a = 70, b = 105, c = 115, d = 104, e = 67;
 
-    printf("%c = %d\n", a, a);
+    printf("%c%c%c%c%c\n", a, b, c, d, e);
 
     return 0;
 }

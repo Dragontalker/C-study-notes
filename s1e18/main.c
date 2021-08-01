630a5ee176228a1b6a13fb5be35871eca78e7214
Richard <richard.yang.tong@gmail.com>
2021-08-01 16:52:22 -0400

    Added notes.

--- s1e18/main.c
+++ s1e18/main.c
@@ -8,12 +8,18 @@ int main()
     printf("Please enter number of character: ");
     scanf("%d", &n);
 
-    char a[n];
+    char a[n + 1];
 
-    printf("Please enter character: ");
-    for (int i = 0; i < n; i++) {
+    printf("Please enter your characters: ");
+    getchar();
+    for (int i = 0; i < n; i++)
+    {
         scanf("%c", &a[i]);
     }
+
+    a[n] = '\0';
+
+    printf("The string you entred is: %s", a);
 
     return 0;
 }

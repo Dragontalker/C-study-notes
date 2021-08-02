7a34d70f7fced944e3ebdb460ef11f8eac12f17f
Richard <richard.yang.tong@gmail.com>
2021-08-02 12:12:09 -0400

    Added notes.

--- s1e29/main.c
+++ s1e29/main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int, int);
+void swap(int*, int*);
 
-void swap(int x, int y)
+void swap(int *x, int *y)
 {
     int temp;
 
-    printf("In swap(), before swapping: x = %d, y = %d\n", x, y);
+    printf("In swap(), before swapping: x = %d, y = %d\n", *x, *y);
 
-    temp = x;
-    x = y;
-    y = temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
 
-    printf("In swap(), after swapping: x = %d, y = %d\n", x, y);
+    printf("In swap(), after swapping: x = %d, y = %d\n", *x, *y);
 }
 
 int main()
@@ -22,7 +22,7 @@ int main()
 
     printf("In main(), before swapping: x = %d, y = %d\n", x, y);
 
-    swap(x, y);
+    swap(&x, &y);
 
     printf("In main(), after swapping: x = %d, y = %d\n", x, y);
 

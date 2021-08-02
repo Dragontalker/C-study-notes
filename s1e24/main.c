38d072882e93d87687ae01ad9d1418ea78dfa911
Richard <richard.yang.tong@gmail.com>
2021-08-02 09:34:17 -0400

    Added notes.

--- s1e24/main.c
+++ s1e24/main.c
@@ -4,10 +4,20 @@
 int main()
 {
     int array[4][5] = {0};
+    int i, j, k = 0;
 
-    printf("sizeof int: %d\n", sizeof(int));
-    printf("address of array: %p\n", array);
-    printf("array = 1: %p\n", array + 1);
+    for (i = 0; i < 4; i++) {
+
+        for (j = 0; j < 4; j++) {
+
+            array[i][j] = k++;
+        }
+    }
+
+    printf("*(array + 1): %p\n", *(array + 1));
+    printf("array[1]: %p\n", array[1]);
+    printf("&array[1][0]: %p\n", &array[1][0]);
+    printf("**(array + 1): %d\n", **(array + 1));
 
     return 0;
 }

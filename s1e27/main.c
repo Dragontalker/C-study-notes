e5f0292daa0d761dfbecfbd5ad171cf8fffceb10
Richard <richard.yang.tong@gmail.com>
2021-08-02 11:28:20 -0400

    Added notes.

--- s1e27/main.c
+++ s1e27/main.c
@@ -5,9 +5,9 @@ int main()
 {
     int num = 520;
     const int cnum = 880;
-    int * const p = &num;
+    const int * const p = &cnum;
 
-    printf("num: %d, &num: %p\n", num, &num);
+    printf("cnum: %d, &cnum: %p\n", cnum, &cnum);
     printf("*pc: %d, pc: %p\n", *p, p);
 
     return 0;

f4f6e5b6c42a58110ec73b820132108518e3eaa2
Richard <richard.yang.tong@gmail.com>
2021-08-02 15:18:36 -0400

    Added notes.

--- s1e29/main.c
+++ s1e29/main.c
@@ -16,15 +16,22 @@ void swap(int *x, int *y)
     printf("In swap(), after swapping: x = %d, y = %d\n", *x, *y);
 }
 
+void get_array(int a[10]);
+
+void get_array(int a[])
+{
+    int i;
+
+    for (i = 0; i < 10; i++) {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
+}
+
 int main()
 {
-    int x = 3, y = 5;
+    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-    printf("In main(), before swapping: x = %d, y = %d\n", x, y);
-
-    swap(&x, &y);
-
-    printf("In main(), after swapping: x = %d, y = %d\n", x, y);
+    get_array(a);
 
     return 0;
 }

76e987fba77bebb0b4bb149b216c88a81f5e7bcb
Richard <richard.yang.tong@gmail.com>
2021-08-02 11:39:59 -0400

    Added notes.

--- s1e28/main.c
+++ s1e28/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_c();
+
+void print_c()
+{
+    printf(" ###### \n");
+    printf("##    ##\n");
+    printf("##      \n");
+    printf("##      \n");
+    printf("##      \n");
+    printf("##    ##\n");
+    printf(" ###### \n");
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    print_c();
+
     return 0;
 }

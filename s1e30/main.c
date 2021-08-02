22c46da271dae53aae2e42f4e8d62872dd7352ff
Richard <richard.yang.tong@gmail.com>
2021-08-02 15:48:08 -0400

    Added notes.

--- s1e30/main.c
+++ s1e30/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+char *getWord(char);
+
+char *getWord(char c)
+{
+    switch(c)
+    {
+        case 'A': return "Apple";
+        case 'B': return "Banana";
+        case 'C': return "Cat";
+        case 'D': return "Dog";
+        default: return "None";
+    }
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    char input;
+
+    printf("Please enter a letter: ");
+    scanf("%c", &input);
+
+    printf("%s\n", getWord(input));
+
     return 0;
 }

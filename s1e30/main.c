ecdbc98560bb5b8be28d19c7d007f83314feb5e5
Richard <richard.yang.tong@gmail.com>
2021-08-02 16:19:47 -0400

    Added notes.

--- s1e30/main.c
+++ s1e30/main.c
@@ -2,7 +2,8 @@
 
 int add(int, int);
 int sub(int, int);
-int calculation(int (*fp)(int, int), int, int);
+int calc(int (*fp)(int, int), int, int);
+int (*select(char op))(int, int);
 
 int add(int num1, int num2)
 {
@@ -14,15 +15,31 @@ int sub(int num1, int num2)
     return num1 - num2;
 }
 
-int calculation(int (*fp)(int, int), int num1, int num2)
+int calc(int (*fp)(int, int), int num1, int num2)
 {
     return (*fp)(num1, num2);
 }
 
+int (*select(char op))(int, int)
+{
+    swtich(op)
+    {
+        case '+': return add;
+        case '-': return sub;
+    }
+}
+
 int main()
 {
-    printf("3 + 5 = %d\n", calculation(add, 3, 5));
-    printf("3 - 5 = %d\n", calculation(sub, 3, 5));
+    int num1, num2;
+    char op;
+    int (*fp)(int, int);
+
+    printf("Please enter an expression, such as 1+3");
+    scanf("%d%c%d", &num1, &op, &num2);
+
+    fp = select(op);
+    printf("%d %c %d = %d\n", num1, op, num2, calc(fp, num1, num2));
 
     return 0;
 }

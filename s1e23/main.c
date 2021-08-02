99c3dd98ca53d05004116d5a4891d6dbe8ae20ba
Richard <richard.yang.tong@gmail.com>
2021-08-01 21:50:33 -0400

    Added notes.

--- s1e23/main.c
+++ s1e23/main.c
@@ -3,19 +3,18 @@
 
 int main()
 {
-    int a = 1;
-    int b = 2;
-    int c = 3;
-    int d = 4;
-    int e = 5;
-
-    int *p1[5] = {&a, &b, &c, &d, &e};
+    char *p1[5] = {
+        "Let programming change the world! -- FishC Studio",
+        "Just do it! -- NIKE",
+        "Everything is possible! -- LiNing",
+        "Never stop! -- AnTa",
+        "One more thing! -- Apple"
+    };
 
     int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d\n", *p1[i]);
+    for (i = 0; i < 5; i++) {
+        printf("%s\n", p1[i]);
     }
 
     return 0;

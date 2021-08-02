5b833326fc36e03010f50c2bab208fd731a5a940
Richard <richard.yang.tong@gmail.com>
2021-08-01 21:14:13 -0400

    Added notes.

--- s1e22/main.c
+++ s1e22/main.c
@@ -3,15 +3,15 @@
 
 int main()
 {
-    char str[128];
+    char a[] = "FishC";
+    int b[5] = {1, 2, 3, 4, 5};
+    float c[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    double d[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
 
-    printf("Please enter url of FishC: ");
-    scanf("%s", str);
-
-    //printf("The URL of FishC Studio is: %s\n", str);
-
-    printf("The address of str is: %p\n", str);
-    printf("The address of the first element in str is: %p\n", &str[0]);
+    printf("the address of a[0] = %p, a[1] = %p, a[2] = %p\n", &a[0], &a[1], &a[2]);
+    printf("the address of b[0] = %p, b[1] = %p, b[2] = %p\n", &b[0], &b[1], &b[2]);
+    printf("the address of c[0] = %p, c[1] = %p, c[2] = %p\n", &c[0], &c[1], &c[2]);
+    printf("the address of d[0] = %p, d[1] = %p, d[2] = %p\n", &d[0], &d[1], &d[2]);
 
     return 0;
 }

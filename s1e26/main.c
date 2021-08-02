99da4728abf31356b1c65ad51b001e550475e565
Richard <richard.yang.tong@gmail.com>
2021-08-02 10:44:18 -0400

    Added notes.

--- s1e26/main.c
+++ s1e26/main.c
@@ -3,32 +3,13 @@
 
 int main()
 {
-    char *cBooks[] = {
-        "<C Programming Language>",
-        "<C Professional Programming>",
-        "<C and Pointers>",
-        "<C: Traps and Pitfalls>",
-        "<C Primer Plus>",
-        "<Learn C with FishC!>"
-    };
-
-    char **byFishC;
-    char **jiayuLoves[4];
-
-    byFishC = &cBooks[5];
-    jiayuLoves[0] = &cBooks[0];
-    jiayuLoves[1] = &cBooks[1];
-    jiayuLoves[2] = &cBooks[2];
-    jiayuLoves[3] = &cBooks[3];
-
-    printf("Published by FishC: %s\n", *byFishC);
-    printf("JiaYu's favorite books: \n");
-
+    int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int *p = array;
     int i;
 
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < 10; i++)
     {
-        printf("%s\n", *jiayuLoves[i]);
+        printf("%d\n", *(p + i));
     }
 
     return 0;

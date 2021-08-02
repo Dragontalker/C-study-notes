61e315a2b5bfebebebf06bfdcc85cee6daf33b43
Richard <richard.yang.tong@gmail.com>
2021-08-01 21:31:48 -0400

    Added notes.

--- s1e22/main.c
+++ s1e22/main.c
@@ -3,18 +3,16 @@
 
 int main()
 {
-    char a[] = "FishC";
-    int b[5] = {1, 2, 3, 4, 5};
-    float c[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
-    double d[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    char *str = "I love FichC.com!";
+    int i, length;
 
-    //int *p = b;
-    printf("*b = %d, *(b+1) = %d, *(b+2) = %d\n", *b, *(b+1), *(b+2));
+    length = strlen(str);
 
-    printf("the address of a[0] = %p, a[1] = %p, a[2] = %p\n", &a[0], &a[1], &a[2]);
-    printf("the address of b[0] = %p, b[1] = %p, b[2] = %p\n", &b[0], &b[1], &b[2]);
-    printf("the address of c[0] = %p, c[1] = %p, c[2] = %p\n", &c[0], &c[1], &c[2]);
-    printf("the address of d[0] = %p, d[1] = %p, d[2] = %p\n", &d[0], &d[1], &d[2]);
+    for (i = 0; i < length; i++)
+    {
+        printf("%c", str[i]);
+    }
+    printf("\n");
 
     return 0;
 }

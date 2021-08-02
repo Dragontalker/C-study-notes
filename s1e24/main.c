f50c2b858ac8ffaf017ef484c9fcbd3043c1c27c
Richard <richard.yang.tong@gmail.com>
2021-08-02 09:43:32 -0400

    Added notes.

--- s1e24/main.c
+++ s1e24/main.c
@@ -3,23 +3,12 @@
 
 int main()
 {
-    int array[4][5] = {0};
-    int i, j, k = 0;
+    int array[2][3] = {{0, 1, 2}, {3, 4, 5}};
+    int (*p)[3] = array;
 
-    for (i = 0; i < 4; i++) {
-
-        for (j = 0; j < 5; j++) {
-
-            array[i][j] = k++;
-        }
-    }
-
-    printf("*(array + 1): %p\n", *(array + 1));
-    printf("array[1]: %p\n", array[1]);
-    printf("&array[1][0]: %p\n", &array[1][0]);
-    printf("**(array + 1): %d\n", **(array + 1));
-    printf("*(*(array + 1) + 3): %d\n", *(*(array + 1) + 3));
-    printf("array[1][3]: %d\n", array[1][3]);
+    printf("**(p+1): %d\n",**(p+1));
+    printf("**(array+1): %d\n", **(array+1));
+    printf("array[1][0]: %d\n", array[1][0]);
 
     return 0;
 }

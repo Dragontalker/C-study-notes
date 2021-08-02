952d3ae0da3d312acbd58938bb58c8898ddca7ef
Richard <richard.yang.tong@gmail.com>
2021-08-02 16:47:38 -0400

    Added notes.

--- s1e32/main.c
+++ s1e32/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-    printf("Hello world!\n");
+    int i = 100; // i1
+    {
+        int i = 110; // i2
+        {
+            int i = 120; // i3
+            printf("i = %d\n", i);
+        }
+        {
+            int i = 130; // i4
+            printf("i = %d\n", i);
+        }
+    }
     return 0;
 }

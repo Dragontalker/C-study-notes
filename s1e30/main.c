849012a74cec7f76513e5a8c5e2c433ddc1e5d88
Richard <richard.yang.tong@gmail.com>
2021-08-02 16:02:21 -0400

    Added notes.

--- s1e30/main.c
+++ s1e30/main.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
-int square(int);
+int add(int, int);
+int sub(int, int);
+int calculation(int (*fp)(int, int), int, int);
 
-int square(int num)
+int add(int num1, int num2)
 {
-    return num * num;
+    return num1 + num2;
+}
+
+int sub(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int calculation(int (*fp)(int, int), int num1, int num2)
+{
+    return (*fp)(num1, num2);
 }
 
 int main()
 {
-    int num;
-    int (*fp)(int);
-
-    printf("Please enter an integer: ");
-    scanf("%d", &num);
-
-    fp = square;
-
-    printf("%d * %d = %d\n", num, num, (*fp)(num));
+    printf("3 + 5 = %d\n", calculation(add, 3, 5));
+    printf("3 - 5 = %d\n", calculation(sub, 3, 5));
 
     return 0;
 }

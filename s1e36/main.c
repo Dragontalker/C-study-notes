257d68f697e5519005b99c2abf8df3b2df3fccab
Richard <richard.yang.tong@gmail.com>
2021-08-03 11:33:01 -0400

    Added notes.

--- s1e36/main.c
+++ s1e36/main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+void quick_sort(int array[], int left, int right)
 {
-    printf("Hello world!\n");
+    int i = left, j = right;
+    int temp;
+    int pivot;
+
+    pivot = array[(left + right) / 2];
+
+    while (i <= j)
+    {
+        /* From left to right: find element bigger than or equal to pivort point */
+        while (array[i] < pivot)
+        {
+            i++;
+        }
+
+        /* From right to left: find element less than pivot point */
+        while (array[j] > pivot)
+        {
+            j--;
+        }
+        if (i <= j)
+        {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j++;
+        }
+    }
+
+    if (left < j)
+    {
+        quick_sort(array, left, j);
+    }
+
+    if (i < right)
+    {
+        quick_sort(array, i, right);
+    }
+}
+
+
+
+
+int main(void)
+{
+    int array[] = {73, 108, 111, 1118, 101, 70, 105, 115, 104, 67, 46, 99, 111, 109};
+    int i, length;
+
+    length = sizeof(array) / sizeof(array[0]);
+
+    quick_sort(array, 0, length - 1);
+
+    printf("After quick sort, result is: ");
+
+    for (i = 0; i < length; i++) {
+        printf(" [#%d, %d] ", i, array[i]);
+    }
+
     return 0;
 }

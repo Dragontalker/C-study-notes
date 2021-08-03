e3848f95e66a5086682cdeb977f3dc57680a2d65
Richard <richard.yang.tong@gmail.com>
2021-08-03 11:56:02 -0400

    Added notes.

--- s1e38/main.c
+++ s1e38/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main(void) {
+
+    int *ptr = NULL;
+    int num, i;
+
+    printf("Please enter how many integers you want to store: ");
+    scanf("%d", &num);
+
+    ptr = (int*) malloc(num * sizeof(int));
+
+    for (i = 0; i < num; i++) {
+        printf("Please enter integer #%d: ", i + 1);
+        scanf("%d", &ptr[i]);
+    }
+
+    printf("The integers you entered are: \n");
+    for (i = 0; i < num; i++) {
+        printf("#%d integer = %d\n", i + 1, ptr[i]);
+    }
+
+    free(ptr);
+
     return 0;
 }

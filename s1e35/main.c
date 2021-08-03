d43aa5c7a862d40921f19af1977ce174b2e2d874
Richard <richard.yang.tong@gmail.com>
2021-08-03 11:11:33 -0400

    Added notes.

--- s1e35/main.c
+++ s1e35/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+void hanoi(int, char, char, char);
+
+void hanoi(int n, char x, char y, char z)
 {
-    printf("Hello world!\n");
+    if (n == 1)
+    {
+        printf("%c --> %c\n", x, z);
+    }
+    else
+    {
+        hanoi(n - 1, x, z, y);
+        printf("%c --> %c\n", x, z);
+        hanoi(n - 1, y, x, z);
+    }
+}
+
+int main(void)
+{
+    int n;
+
+    printf("Please enter the number of stacks on Hanoi: ");
+    scanf("%d", &n);
+
+    hanoi(n, 'X', 'Y', 'Z');
+
     return 0;
 }

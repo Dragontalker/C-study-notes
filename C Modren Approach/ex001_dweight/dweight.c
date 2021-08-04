747a9b8e9853eb0e13ab5ea112c10c139e8d9a87
Richard <richard.yang.tong@gmail.com>
2021-08-04 14:38:32 -0400

    Added notes.

---
+++ C Modren Approach/ex001_dweight/dweight.c
@@ -0,0 +1,22 @@
+/*
+ * Name: dweight.c
+ * Purpose: Computes the dimensional weight of a 12" x 10" x 8" box
+ * Author: Richard T. Yang
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int length = 12;
+    int width = 10;
+    int height = 8;
+
+    int volume = length * width * height;
+    int weight = (volume + 165) / 166;
+
+    printf("Dimensions: %d\" x %d\" x %d\"\n", length, width, height);
+    printf("Volumn (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
+
+    return 0;
+}

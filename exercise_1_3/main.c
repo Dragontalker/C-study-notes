5d42f42f4cf6ee00648cb6fa1bbe03a0d120e1f1
Richard <richard.yang.tong@gmail.com>
2021-08-04 09:57:28 -0400

    Added notes.

--- exercise_1_3/main.c
+++ exercise_1_3/main.c
@@ -1,3 +1,8 @@
+/*
+    Exercise 1-3: Modify the temperature conversion
+    program to print a heading above the table.
+*/
+
 #include <stdio.h>
 
 void main(void)
@@ -10,9 +15,10 @@ void main(void)
     step = 20;
 
     fahr = lower;
+    printf("Fahrenheit\t Celsuis\n");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%3.0f\t\t %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }

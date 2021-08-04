68b004ee84d64fcdc8620d4b8a25cf3d26a68e75
Richard <richard.yang.tong@gmail.com>
2021-08-04 15:28:32 -0400

    Added notes.

--- C Modren Approach/ex003_celsius/celsius.c
+++ C Modren Approach/ex003_celsius/celsius.c
@@ -1,7 +1,14 @@
+/*
+ * Name: celsius.c
+ * Purpose: Converts a Fahreheit temperature to Celsuis.
+ * Author: Richard T. Yang
+ */
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main(void)
 {
     printf("Hello world!\n");
     return 0;

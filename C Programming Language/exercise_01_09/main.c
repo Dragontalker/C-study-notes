9d3866b93c0a71f3bb8695030731018fb6be2156
Richard <richard.yang.tong@gmail.com>
2021-08-04 10:56:51 -0400

    Added notes.

--- C Programming Language/exercise_01_09/main.c
+++ C Programming Language/exercise_01_09/main.c
@@ -1,8 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+    Exercise 1-10:
+    Write a program to copy its input to its output, replacing
+    each string of one or more blanks by a single blank.
+*/
 
-int main()
+#include <stdio.h>
+
+int main(void)
 {
-    printf("Hello world!\n");
+    int c, blank_count;
+
+    blank_count = 0;
+
+    while ((c = getchar()) != EOF) {
+
+        if (c == ' ') {
+            if (blank_count == 0) {
+                putchar(c);
+            }
+            blank_count++;
+        } else {
+            blank_count = 0;
+            putchar(c);
+        }
+    }
+
     return 0;
 }

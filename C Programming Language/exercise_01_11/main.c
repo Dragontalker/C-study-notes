8096ba67f3f8bc0d1d7d2a4d32577796e67b4647
Richard <richard.yang.tong@gmail.com>
2021-08-04 12:05:17 -0400

    Added notes.

--- C Programming Language/exercise_01_11/main.c
+++ C Programming Language/exercise_01_11/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+#define IN 1    /* inside a word */
+#define OUT 0   /* outside a word */
+
+/* count lines, words, and characters in input */
 int main()
 {
-    printf("Hello world!\n");
+    int input;
+
+    int state;
+
+    int line_count, word_count, character_count;
+
+    state = OUT;
+
+    line_count = word_count = character_count = 0;
+
+    while ((input = getchar()) != EOF) {
+        ++character_count;
+
+        if (input == '\n')
+            ++line_count;
+
+        if (input == ' ' || input == '\n' || input == '\t') {
+            state = OUT;
+        } else if (state == OUT) {
+            state = IN;
+            word_count++;
+        }
+    }
+
+    printf("The total line count = %d\n", line_count);
+    printf("The total word count = %d\n", word_count);
+    printf("The total character count = %d\n", character_count);
+
     return 0;
 }

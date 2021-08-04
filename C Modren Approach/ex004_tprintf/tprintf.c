2ed133c0844e7c2f757eb006e8f0d5eef7a600d9
Richard <richard.yang.tong@gmail.com>
2021-08-04 17:34:17 -0400

    Added notes.

--- C Modren Approach/ex004_tprintf/tprintf.c
+++ C Modren Approach/ex004_tprintf/tprintf.c
@@ -7,6 +7,11 @@
 
 int main(void)
 {
+    int i = 40;
+    float x = 839.21f;
+
+    printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i, i);
+    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x, x);
 
     return 0;
 }

10a273241a17ef5f4ab76afc72b09c23ffd3d9b4
Richard <richard.yang.tong@gmail.com>
2021-08-04 15:10:07 -0400

    Added notes.

--- C Modren Approach/ex002_dweight2/main.c
+++ C Modren Approach/ex002_dweight2/main.c
@@ -1,8 +1,29 @@
+/*
+ * Name: dweight2.c
+ * Purpose: Computes the dimensional weight of a box from input provided by the user.
+ * Author: Richard T. Yang
+ */
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+int main(void)
 {
-    printf("Hello world!\n");
+    int length, width, height;
+
+    printf("Enter length of box: ");
+    scanf("%d", &length);
+
+    printf("Enter width of box: ");
+    scanf("%d", &width);
+
+    printf("Enter height of box: ");
+    scanf("%d", &height);
+
+    int volume = length * width * height;
+    int weight = (volume + 165) / 166;
+
+    printf("Dimensions: %d\" x %d\" x %d\"\n", length, width, height);
+    printf("Volumn (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
+
     return 0;
 }

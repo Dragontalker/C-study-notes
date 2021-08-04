e84a3eadc3c47e0c5984b7f6daeb95b1c44b0422
Richard <richard.yang.tong@gmail.com>
2021-08-04 10:12:21 -0400

    Added notes.

--- C Programming Language/exercise_01_05/main.c
+++ C Programming Language/exercise_01_05/main.c
@@ -1,8 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+    Exercise 1-5:
+    Modify the temperature conversion program to print the table
+    in reverse order, that is, from 300 degress to 0.
+*/
 
-int main()
+#include <stdio.h>
+
+void main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+    int celsius;
+
+    for (celsius = 300; celsius >= 0; celsius = celsius - 20) {
+        printf("%6d %3.1f\n", celsius, (9.0/5.0) * celsius + 32);
+    }
 }

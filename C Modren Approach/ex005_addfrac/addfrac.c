8bcb39e12d355fce5833d9ed426102da769da8a2
Richard <richard.yang.tong@gmail.com>
2021-08-04 17:49:27 -0400

    Added notes.

--- C Modren Approach/ex005_addfrac/addfrac.c
+++ C Modren Approach/ex005_addfrac/addfrac.c
@@ -7,6 +7,15 @@
 
 int main(void)
 {
+    int num1, denom1;
+
+    printf("Please enter the first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+
+    int num2, denom2;
+
+    printf("Please enter the second fraction: ");
+    scanf("%d/%d", &num2, &denom2);
 
 
     return 0;

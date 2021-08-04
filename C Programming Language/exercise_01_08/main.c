f75b2a6a9aad2911d242387972e5c0d81b022d37
Richard <richard.yang.tong@gmail.com>
2021-08-04 10:44:39 -0400

    Added notes.

--- C Programming Language/exercise_01_08/main.c
+++ C Programming Language/exercise_01_08/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+void main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+    int c, blank_count, tab_count, newline_count;
+
+    blank_count = tab_count = newline_count = 0;
+
+    while ((c = getchar()) != EOF) {
+
+        if (c == ' ') {
+            ++blank_count;
+        }
+
+        if (c == '\t') {
+            ++tab_count;
+        }
+
+        if (c == '\n') {
+            ++newline_count;
+        }
+    }
+
+    printf("The total count of blanks = %d\n", blank_count);
+    printf("The total count of tabs = %d\n", tab_count);
+    printf("The total count of new lines = %d\n", newline_count);
 }

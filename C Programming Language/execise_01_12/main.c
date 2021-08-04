ccedfa60cf9035ae87c4278f6f6c549b592fc343
Richard <richard.yang.tong@gmail.com>
2021-08-04 12:11:54 -0400

    Added notes.

--- C Programming Language/execise_01_12/main.c
+++ C Programming Language/execise_01_12/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NEW 1
+#define NOT 0
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int c, state;
+
+    state = NEW;
+
+    while ((c = getchar()) != EOF) {
+        putchar(c);
+    }
 }

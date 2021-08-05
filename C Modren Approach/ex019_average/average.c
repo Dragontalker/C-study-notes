b55bfd27aee9be5805fa7d10b725e83b2d00712d
Richard <richard.yang.tong@gmail.com>
2021-08-05 17:05:47 -0400

    Added notes.

--- C Modren Approach/ex019_average/average.c
+++ C Modren Approach/ex019_average/average.c
@@ -11,6 +11,13 @@ double average(double a, double b) {
 
 int main()
 {
+    double x, y, z;
+    printf("Enter three numbers: ");
+    scanf("%lf%lf%lf", &x, &y, &z);
+
+    printf("Average of %g and %g: %g\n", x, y, average(x, y));
+    printf("Average of %g and %g: %g\n", y, z, average(y, z));
+    printf("Average of %g and %g: %g\n", x, z, average(x, z));
 
     return 0;
 }

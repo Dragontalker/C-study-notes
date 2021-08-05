0ab35ab6a5133864c835659a5c39c4870f43cc4c
Richard <richard.yang.tong@gmail.com>
2021-08-05 11:01:44 -0400

    Added notes.

--- C Modren Approach/ex008_date/main.c
+++ C Modren Approach/ex008_date/main.c
@@ -11,7 +11,22 @@ int main(void)
     printf("Please enter the date (mm/dd/yy): ");
     scanf("%d/%d/%d", &month, &day, &year);
 
+    printf("The legal format is %d", day);
 
+    switch (day) {
+        case 1: case 21: case 31:
+            printf("st");
+            break;
+        case 2: case 22:
+            printf("nd");
+            break;
+        case 3: case 23:
+            printf("rd");
+            break;
+        default:
+            printf("th");
+            break;
+    }
 
     return 0;
 }

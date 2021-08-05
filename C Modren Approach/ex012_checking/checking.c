06f6ad9929deb9152c626367563df7786dc86d67
Richard <richard.yang.tong@gmail.com>
2021-08-05 11:54:15 -0400

    Added notes.

--- C Modren Approach/ex012_checking/checking.c
+++ C Modren Approach/ex012_checking/checking.c
@@ -7,6 +7,41 @@
 
 int main(void)
 {
+    printf("*** ACME checkbook-balancing program ***\n");
+    printf("Commands: 0=clear, 1=credit, 2=debit, 3=balance, 4=exit\n");
+
+    int cmd;
+    float credit, debit;
+    float balance = 0.0f;
+
+    for (;;) {
+        printf("Please enter a command: ");
+        scanf("%d", &cmd);
+
+        switch (cmd) {
+            case 0:
+                balance = 0.0f;
+                break;
+            case 1:
+                printf("Please enter the amount of credit: ");
+                scanf("%f", &credit);
+                balance += credit;
+                break;
+            case 2:
+                printf("Please enter the amount of debit: ");
+                scanf("%f", &debit);
+                balance -= debit;
+                break;
+            case 3:
+                printf("Your current balance is: $%.2f\n", balance);
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("Commands: 0=clear, 1=credit, 2=debit, 3=balance, 4=exit\n");
+                break;
+        }
+    }
 
     return 0;
 }

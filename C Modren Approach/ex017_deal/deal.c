980b6f67d9776d6d6efe119ee22711acff105d49
Richard <richard.yang.tong@gmail.com>
2021-08-05 16:43:29 -0400

    Added notes.

--- C Modren Approach/ex017_deal/deal.c
+++ C Modren Approach/ex017_deal/deal.c
@@ -3,7 +3,13 @@
  * Purpose: Deals a random hand of cards.
  * Author: Richard T. Yang
  */
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_SUITS 4
+#define NUM_RANKS 13
 
 int main(void)
 {

a1c64bd0f54633c95c8e7734b20ecbd0539728fa
Richard <richard.yang.tong@gmail.com>
2021-08-05 16:56:43 -0400

    Added notes.

--- C Modren Approach/ex018_reverse2/reverse2.c
+++ C Modren Approach/ex018_reverse2/reverse2.c
@@ -7,6 +7,22 @@
 
 int main(void)
 {
+    int n;
+    printf("How many numbers do you want to reverse? ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enterd %d numbers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("In reverse order: ");
+    for (int i = n - 1; i >= 0; i--) {
+        printf(" %d", a[i]);
+    }
+    printf("\n");
 
     return 0;
 }

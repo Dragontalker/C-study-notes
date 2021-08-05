00005efc0b62721b555adb571e14ed412386e762
Richard <richard.yang.tong@gmail.com>
2021-08-05 17:12:38 -0400

    Added notes.

--- C Modren Approach/ex020_prime/prime.c
+++ C Modren Approach/ex020_prime/prime.c
@@ -3,7 +3,23 @@
  * Purpose: Tests whether a number is prime.
  * Author: Richard T. Yang
  */
+#include <stdbool.h>
 #include <stdio.h>
+
+bool is_prime(int n) {
+
+    if (n <= 1) {
+        return false;
+    }
+
+    for (int divisor = 2; divisor * divisor <= n; divisor++) {
+        if (n % divisor == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main(void)
 {

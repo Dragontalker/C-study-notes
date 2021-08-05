4aeff448058af3df3c6f6116d5ad43f31636912c
Richard <richard.yang.tong@gmail.com>
2021-08-05 10:44:34 -0400

    Added notes.

--- C Modren Approach/ex007_broker/main.c
+++ C Modren Approach/ex007_broker/main.c
@@ -7,6 +7,31 @@
 
 int main(void)
 {
+    float value;
+    printf("Please enter the value of trade: ");
+    scanf("%f", &value);
+
+    float commission;
+
+    if (value < 2500.00f) {
+        commission = 30.00f + .017f * value;
+    } else if (value < 6250.00f) {
+        commission = 56.00f + .006f * value;
+    } else if (value < 20000.00f) {
+        commission = 76.00f + .0034f * value;
+    } else if (value < 50000.00f) {
+        commission = 100.00f + .0022f * value;
+    } else if (value < 500000.00f) {
+        commission = 155.00f + .0011f * value;
+    } else {
+        commission = 255.00f + .0009f * value;
+    }
+
+    if (commission < 39.00f) {
+        commission = 39.00f;
+    }
+
+    printf("The commission is: $%.2f\n", commission);
 
     return 0;
 }

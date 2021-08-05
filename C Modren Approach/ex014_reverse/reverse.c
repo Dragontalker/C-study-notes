d9230b737851266c71c87ba8f356f6df8881d760
Richard <richard.yang.tong@gmail.com>
2021-08-05 15:59:11 -0400

    Added notes.

--- C Modren Approach/ex014_reverse/reverse.c
+++ C Modren Approach/ex014_reverse/reverse.c
@@ -5,8 +5,24 @@
  */
 #include <stdio.h>
 
+#define N 10
+
 int main(void)
 {
+    int a[N];
+
+    printf("Enter %d numbers: ", N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("In reverse order: ");
+
+    for (int i = N - 1; i >= 0; i--) {
+        printf(" %d", a[i]);
+    }
+    printf("\n");
 
     return 0;
 }

82d815cef6bae2849291473f0e1615260c1e2a4c
Richard <richard.yang.tong@gmail.com>
2021-08-05 16:07:14 -0400

    Added notes.

--- C Modren Approach/ex015_repdigit/repdigit.c
+++ C Modren Approach/ex015_repdigit/repdigit.c
@@ -3,10 +3,36 @@
  * Purpose: Check numbers for repeated digits.
  * Author: Richard T. Yang
  */
+#include <stdbool.h>
 #include <stdio.h>
 
 int main(void)
 {
+    long n;
+    printf("Please enter a number: ");
+    scanf("%ld", &n);
+
+    bool digit_seen[10] = {false};
+
+    int digit;
+
+    while (n > 0) {
+
+        digit = n % 10;
+
+        if (digit_seen[digit]) {
+            break;
+        }
+
+        digit_seen[digit] = true;
+        n /= 10;
+    }
+
+    if (n > 0) {
+        printf("Repeated digit...\n");
+    } else {
+        printf("No repeated digit...\n");
+    }
 
     return 0;
 }

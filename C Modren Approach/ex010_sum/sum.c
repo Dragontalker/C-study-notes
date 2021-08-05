2e518f7132b4c96617970757783a08d29741bbd4
Richard <richard.yang.tong@gmail.com>
2021-08-05 11:30:44 -0400

    Added notes.

--- C Modren Approach/ex010_sum/sum.c
+++ C Modren Approach/ex010_sum/sum.c
@@ -7,6 +7,19 @@
 
 int main(void)
 {
+    printf("This program sums a series of integers.\n");
+
+    int n;
+    printf("Please enter integers (0 to terminate): ");
+    scanf("%d", &n);
+
+    int sum = 0;
+    while (n != 0) {
+        sum += n;
+        scanf("%d", &n);
+    }
+
+    printf("The sum is: %d\n.", sum);
 
     return 0;
 }

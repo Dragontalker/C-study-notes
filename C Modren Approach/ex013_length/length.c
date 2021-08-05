88445327586b72a07309f525b88f200e958397c5
Richard <richard.yang.tong@gmail.com>
2021-08-05 15:29:19 -0400

    Added notes.

--- C Modren Approach/ex013_length/length.c
+++ C Modren Approach/ex013_length/length.c
@@ -7,6 +7,17 @@
 
 int main(void)
 {
+    char ch;
+    printf("Please enter a message: ");
+    ch = getchar();
+
+    int len = 0;
+    while (ch != '\n') {
+        len++;
+        ch = getchar();
+    }
+
+    printf("Your message was %d character(s) long.\n", len);
 
     return 0;
 }

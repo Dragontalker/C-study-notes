dbb09cc2740c50b4052ff26fa74e025f0a8a9a5f
Richard <richard.yang.tong@gmail.com>
2021-08-05 16:53:53 -0400

    Added notes.

---
+++ C Modren Approach/ex018_reverse2/reverse2.c
@@ -0,0 +1,12 @@
+/*
+ * Name: reverse2.c
+ * Purpose: Reverses a series of numbers using a variable-length array - C99 only.
+ * Author: Richard T. Yang
+ */
+#include <stdio.h>
+
+int main(void)
+{
+
+    return 0;
+}

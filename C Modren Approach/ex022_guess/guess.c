533140ad3702cf6ea3828b5375fbdaef50b09aac
Richard <richard.yang.tong@gmail.com>
2021-08-06 09:19:28 -0400

    Added notes.

--- C Modren Approach/ex022_guess/guess.c
+++ C Modren Approach/ex022_guess/guess.c
@@ -5,6 +5,17 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define MAX_NUMBER 100
+
+/* external vairable */
+int secret_number;
+
+/* prototypes */
+void initialize_number_generator(void);
+void choose_new_secret_number(void);
+void read_guesses(void);
 
 int main()
 {

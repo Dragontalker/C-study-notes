1b696eae30ca65703f5dcfa17189e1069fe4b957
Richard <richard.yang.tong@gmail.com>
2021-08-06 10:06:32 -0400

    Added notes.

--- C Modren Approach/ex023_maxmin/maxmin.c
+++ C Modren Approach/ex023_maxmin/maxmin.c
@@ -12,6 +12,18 @@ void max_min(int [], int, int *, int *)
 
 int main(void)
 {
+    int b[N], big, small;
+
+    printf("Enter %d numbers: ", N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    max_min(b, N, &big, &small);
+
+    printf("Largest: %d\n", big);
+    printf("Smallest: %d\n", small);
 
     return 0;
 }

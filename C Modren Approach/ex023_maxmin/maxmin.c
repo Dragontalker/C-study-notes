43bd8eef51e5de994b720cc334bcd4e2d986dabc
Richard <richard.yang.tong@gmail.com>
2021-08-06 10:15:25 -0400

    Added notes.

--- C Modren Approach/ex023_maxmin/maxmin.c
+++ C Modren Approach/ex023_maxmin/maxmin.c
@@ -8,7 +8,7 @@
 #define N 10
 
 /* prototype */
-void max_min(int [], int, int *, int *)
+void max_min(int [], int, int*, int*);
 
 int main(void)
 {
@@ -26,4 +26,17 @@ int main(void)
     printf("Smallest: %d\n", small);
 
     return 0;
+}
+
+void max_min(int a[], int n, int *max, int *min)
+{
+    *max = *min = a[0];
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] > *max) {
+            *max = a[i];
+        } else if (a[i] < max) {
+            *min = a[i];
+        }
+    }
 }

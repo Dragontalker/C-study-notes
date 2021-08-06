52a140455114d4b003141fbce0dc83c21a96c0db
Richard <richard.yang.tong@gmail.com>
2021-08-06 18:03:17 -0400

    Added notes.

--- C Modren Approach/ex025_planet/planet.c
+++ C Modren Approach/ex025_planet/planet.c
@@ -4,9 +4,26 @@
  * Author: Richard T. Yang
  */
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+#define NUM_PLANETS 9
+
+int main(int argc, char *argv[])
 {
+    char *planets[] = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"};
+
+
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; j < NUM_PLANETS; j++) {
+            if (strcmp(argv[i], planets[i]) == 0) {
+                printf("%s is planet %d\n", argv[i], j + 1);
+                break;
+            }
+            if (j == NUM_PLANETS) {
+                printf("%s is not a planet\n");
+            }
+        }
+    }
 
     return 0;
 }

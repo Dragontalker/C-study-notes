b2f37749ef6035437159a5578c31d7d21d6be281
Richard <richard.yang.tong@gmail.com>
2021-08-06 09:13:48 -0400

    Added notes.

--- C Modren Approach/ex021_stack/stack.c
+++ C Modren Approach/ex021_stack/stack.c
@@ -16,25 +16,35 @@
  void push(int);
  int pop(void);
 
-int main(void)
-{
+int main(void) {
     return 0;
 }
 
-void make_empty(void)
-{
+void make_empty(void) {
     top=0;
 }
 
-bool is_empty(void)
-{
+bool is_empty(void) {
     return top == 0;
 }
 
-bool is_full(void)
-{
+bool is_full(void) {
     return top == STACK_SIZE;
 }
 
+void push(int i) {
+    if (is_full()) {
+        stack_overflow();
+    } else {
+        contents[top++] = i;
+    }
+}
 
+int pop(void) {
 
+    if (is_empty()) {
+        stack_underflow();
+    } else {
+        return contents[--top];
+    }
+}

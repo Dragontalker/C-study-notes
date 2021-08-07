1911132e6f99931869f91ec0ca8308a3f402f361
Richard <richard.yang.tong@gmail.com>
2021-08-07 15:47:52 -0400

    Added notes.

--- Notes/pointer/main.c
+++ Notes/pointer/main.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 
-void minMax(int [], int, int*, int*);
+int* findMid(int [], int);
 
 int main(void)
 {
-    int a[] = {23, 4, 21, 987, 45, 32, 10, 123, 986, 50, 3, 4, 5};
+    int a[] = {1, 2, 3, 4, 5};
 
-    int min, max;
+    int n = sizeof(a) / sizeof(a[0]);
 
-    int len = sizeof(a) / sizeof(a[0]);
+    int* mid = findMid(a, n);
 
-    minMax(a, len, &min, &max);
-
-    printf("The minimum value in this array = %d\n", min);
-    printf("The maximum value in this array = %d\n", max);
+    printf("%d", *(mid));
 
     return 0;
 }
 
-void minMax(int arr[], int len, int* min, int* max)
+int* findMid(int arr[], int len)
 {
-    *(min) = *(max) = arr[0];
-
-    for (int i = 0; i < len; i++) {
-        if (arr[i] > *(max)) {
-            *(max) = arr[i];
-        }
-        if (arr[i] < *(min)) {
-            *(min) = arr[i];
-        }
-    }
+    return &arr[len/2];
 }

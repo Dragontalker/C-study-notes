b548a028dd80d2a53468df0bee9b3b10e91e3a23
Richard <richard.yang.tong@gmail.com>
2021-08-08 11:45:10 -0400

    Added notes.

--- Notes/pointer_arithmetic/main.c
+++ Notes/pointer_arithmetic/main.c
@@ -10,6 +10,18 @@ int main()
     printf("Address of a[] = %\p\n", a);
     printf("Address of p = %p\n", p);
 
+    printf("\n");
+
+    printf("Address of a[1] = %\p\n", &a[1]);
+    printf("Address of p + 1 = %p\n", p + 1);
+
+    printf("\n");
+
+    printf("Address of a[2] = %\p\n", &a[2]);
+    printf("Address of p + 2 = %p\n", p + 2);
+
+    printf("\n");
+
     printf("Address of a[3] = %\p\n", &a[3]);
     printf("Address of p + 3 = %p\n", p + 3);
 

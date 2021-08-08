bac378cfb1d184b41f3b10752cd3c2720d80a574
Richard <richard.yang.tong@gmail.com>
2021-08-08 14:42:08 -0400

    Added notes.

--- Notes/pointer_arithmetic/main.c
+++ Notes/pointer_arithmetic/main.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
-void f(int*, int*);
+int f(int*, int);
 
 int i = 0;
 int j = 1;
 
 int main()
 {
-    f(&i, &j);
+    int a[] = {12, 7, 13, 4, 11, 6};
 
-    printf("%d %d\n", i, j);
+    printf("%d", f(a, 6)); //12 + (7 - (13 - (4 + (11 - (6 + 0)))))
 
     return 0;
 }
 
-void f(int* p, int* q)
+int f(int* a, int n)
 {
-    p = q;
-    *(p) = 2;
+    if (n <= 0) {
+        return 0;
+    } else if (*(a) % 2 == 0) {
+        return *(a) + f(a+1, n-1);
+    } else {
+        return *(a) - f(a+1, n-1);
+    }
 }
 
 

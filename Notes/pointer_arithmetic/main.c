287de34d0de5f48847b2f2b306f75f3168b8b44e
Richard <richard.yang.tong@gmail.com>
2021-08-08 16:27:59 -0400

    Added notes.

--- Notes/pointer_arithmetic/main.c
+++ Notes/pointer_arithmetic/main.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 
-void swap(int*, int*);
-void printab(void);
-
 int main()
 {
-    printab();
-    printab();
+    char a[2][3] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}};
+    char b[3][2];
+
+    char* p = *(b);
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            *(p + 2*j + i) = a[i][j];
+        }
+    }
+
+    printf("Array a: {");
+    for (int i = 0; i < 2; i++) {
+        printf("{");
+        for (int j = 0; j < 3; j++) {
+            printf(" %c", a[i][j]);
+        }
+        printf(" }");
+    }
+    printf("}\n");
+
+    printf("Array b: {");
+    for (int i = 0; i < 3; i++) {
+        printf("{");
+        for (int j = 0; j < 2; j++) {
+            printf(" %c", b[i][j]);
+        }
+        printf(" }");
+    }
+    printf("}\n");
 
     return 0;
-}
-
-void swap(int* x, int* y)
-{
-    static int* temp;
-    temp = x;
-    x = y;
-    y = temp;
-}
-
-void printab()
-{
-    static int i, a = -3, b = -6;
-
-    i = 0;
-
-    while (i <= 4)
-    {
-        if ((i++)%2 == 1) continue;
-        a = a + i;
-        b = b + i;
-    }
-    swap(&a, &b);
-
-    printf("a = %d, b = %d\n", a, b);
 }

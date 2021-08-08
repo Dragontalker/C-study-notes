c241651576f60df81bd0f8de378eff25a85495f4
Richard <richard.yang.tong@gmail.com>
2021-08-08 19:31:45 -0400

    Added notes.

--- Notes/pointer_arithmetic/main.c
+++ Notes/pointer_arithmetic/main.c
@@ -2,36 +2,11 @@
 
 int main()
 {
-    char a[2][3] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}};
-    char b[3][2];
+    char a[10];
+    printf("Enter the string: \n");
+    gets(a);
 
-    char* p = *(b);
-
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            *(p + 2*j + i) = a[i][j];
-        }
-    }
-
-    printf("Array a: {");
-    for (int i = 0; i < 2; i++) {
-        printf("{");
-        for (int j = 0; j < 3; j++) {
-            printf(" %c", a[i][j]);
-        }
-        printf(" }");
-    }
-    printf("}\n");
-
-    printf("Array b: {");
-    for (int i = 0; i < 3; i++) {
-        printf("{");
-        for (int j = 0; j < 2; j++) {
-            printf(" %c", b[i][j]);
-        }
-        printf(" }");
-    }
-    printf("}\n");
+    printf("%s", a);
 
     return 0;
 }

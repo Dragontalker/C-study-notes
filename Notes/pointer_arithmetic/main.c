ab9dec4e7f715552e2beb7f7c7362fa85d54ee83
Richard <richard.yang.tong@gmail.com>
2021-08-08 12:17:06 -0400

    Added notes.

--- Notes/pointer_arithmetic/main.c
+++ Notes/pointer_arithmetic/main.c
@@ -3,12 +3,17 @@
 
 int main()
 {
-    int a[] = {1, 2, 3, 4, 5, 6, 7};
+    int a[] = {5, 16, 7, 89, 45, 32, 23, 10};
 
-    int* p = &a[2];
+    int* p = &a[1]; //16
+    int* q = &a[5]; //32
 
-    printf("Address of a[4] = %p\n", &a[4]);
-    printf("Address of p + 2 = %p\n", p + 2);
+    printf("%d ", *(p + 3)); //45
+    printf("%d ", *(q - 3)); //7
+    printf("%d ", q - p); //4
+    printf("%d ", p < q); //1
+    printf("%d ", *(p) < *(q)); //1
+
 
     return 0;
 }

1716c46f73cb0b7bfe8ccbe75822ca8c800210ea
Richard <richard.yang.tong@gmail.com>
2021-08-08 11:36:18 -0400

    Added notes.

--- Notes/pointer/main.c
+++ Notes/pointer/main.c
@@ -6,9 +6,13 @@ int main(void)
 {
     const int i = 10;
 
+    printf("i = %d\n", i);
+    printf("address of i = %p\n", &i);
+
     fun(&i);
 
     printf("i is now: %d\n", i);
+    printf("address of i = %p\n", &i);
 
     return 0;
 }

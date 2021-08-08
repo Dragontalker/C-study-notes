f4596fae8373f2a71340d92a674616579f993560
Richard <richard.yang.tong@gmail.com>
2021-08-08 11:42:42 -0400

    Added notes.

--- Notes/pointer_arithmetic/main.c
+++ Notes/pointer_arithmetic/main.c
@@ -3,6 +3,12 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a[] = {1, 2, 3, 4, 5, 6, 7};
+
+    int* p = &a[0];
+
+    printf("Address of a[] = %\p\n", a);
+    printf("Address of p = %p\n", p);
+
     return 0;
 }

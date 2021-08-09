1cccdc758d9ae1889c873ceace32814f0f7be003
Richard <richard.yang.tong@gmail.com>
2021-08-09 15:18:46 -0400

    Added notes.

---
+++ Notes/struct_func/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int age;
+    int roll_no;
+    float marks;
+};
+
+int main(void)
+{
+    struct Student s1 = {"Nick", 16, 50, 72.5};
+
+    printf("Student.name = %s\n", s1.name);
+    printf("Student.age = %d\n", s1.age);
+    printf("Student.roll_no = %d\n", s1.roll_no);
+    printf("Student.marks = %.2f\n", s1.marks);
+
+    return EXIT_SUCCESS;
+}

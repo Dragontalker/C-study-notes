a60cf8880a5ac6fe9ca16ef8a27179c6a25f4306
Richard <richard.yang.tong@gmail.com>
2021-08-09 08:02:21 -0400

    Added notes.

--- Notes/structure/main.c
+++ Notes/structure/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct {
-    char* engine;
-} car1, car2;
+struct abc {
+    int x;
+    int y;
+    int z;
+};
 
 int main()
 {
-    car1.engine = "DDis 190 Engine";
-    car2.engine = "1.2 L Kappa Dual VTVT";
+    struct abc a = {.y = 20, .x = 10, .z = 30};
 
-    printf("%s\n", car1.engine);
-    printf("%s\n", car2.engine);
+    printf("%d %d %d", a.x, a.y, a.z);
 
     return 0;
 }

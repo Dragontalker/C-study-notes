0c8230cb93d097bbfce780daf0c473a412aded73
Richard <richard.yang.tong@gmail.com>
2021-08-09 14:59:25 -0400

    Added notes.

--- Notes/calloc/main.c
+++ Notes/calloc/main.c
@@ -3,6 +3,13 @@
 
 int main(void)
 {
+    int* ptr = (int*) malloc(sizeof(int));
+
+    printf("The address of ptr = %p\n", ptr);
+
+    ptr = (int*) realloc(ptr, 2 * sizeof(int));
+
+    printf("The address of ptr = %p\n", ptr);
 
     return EXIT_SUCCESS;
 }

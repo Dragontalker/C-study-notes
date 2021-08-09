eb2765bed87547acc31a9f9ded6a72dc990059e3
Richard <richard.yang.tong@gmail.com>
2021-08-09 05:27:45 -0400

    Added notes.

--- Notes/string_lib/main.c
+++ Notes/string_lib/main.c
@@ -5,8 +5,10 @@ int main(void)
 {
     char str1[10] = "Hello";
     char str2[10];
+    char str3[10];
 
     printf("%s\n", strcpy(str2, str1));
+    printf("%s\n", strcpy(str3, strcpy(str2, str1)));
     printf("%s\n", str2);
 
     return 0;

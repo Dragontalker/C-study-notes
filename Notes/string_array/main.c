19ddb392454de234ad20206e4ec6abf4dca83061
Richard <richard.yang.tong@gmail.com>
2021-08-09 06:25:56 -0400

    Added notes.

--- Notes/string_array/main.c
+++ Notes/string_array/main.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void foo(char*);
+void func1(char*, char*);
+void func2(char**, char**);
 
 int main()
 {
-    foo("ABCD EFGH");
+    char* str1 = "Hi";
+    char* str2 = "Bye";
+
+    func1(str1, str2);
+    printf("%s %s", str1, str2);
+
+    func2(&str1, &str2);
+    printf("%s %s", str1, str2);
 
     return 0;
 }
 
-void foo(char* a)
+void func1(char* s1, char* s2)
 {
-    if (*(a) && *(a) != ' ')
-    {
-        foo(a+1);
-        putchar(*(a));
-    }
+    char* tmp;
+    tmp = s1;
+    s1 = s2;
+    s2 = tmp;
+}
+
+void func2(char** s1, char** s2)
+{
+    char* tmp;
+    tmp = *s1;
+    *s1 = *s2;
+    *s2 = tmp;
 }

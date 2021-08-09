e1f62e74462e493a8a3c9e7960a626631aa4d648
Richard <richard.yang.tong@gmail.com>
2021-08-09 07:11:04 -0400

    Added notes.

--- Notes/structure/main.c
+++ Notes/structure/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    struct {
+        char* engine;
+        char* fule_type;
+        int fule_tank_cap;
+        int seating_cap;
+        float city_mileage;
+    } car1, car2;
+
     return 0;
 }

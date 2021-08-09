a9554aec066a5cd967ec56f6a60b017437291c94
Richard <richard.yang.tong@gmail.com>
2021-08-09 06:17:37 -0400

    Added notes.

--- Notes/string_array/main.c
+++ Notes/string_array/main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void foo(char*);
+
 int main()
 {
-    char p[20];
-    char* s = "string";
-
-    int length = strlen(s); //6
-    for (int i = 0; i < length; i++)
-        p[i] = s[length - i - 1];
-    p[length] = '\0';
-
-    printf("%s", p);
+    foo("ABCD EFGH");
 
     return 0;
+}
+
+void foo(char* a)
+{
+    if (*(a) && *(a) != ' ')
+    {
+        foo(a+1);
+        putchar(*(a));
+    }
 }

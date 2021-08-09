f1f34b808d7ae023b50c724a26f56aa9df295332
Richard <richard.yang.tong@gmail.com>
2021-08-09 05:58:30 -0400

    Added notes.

---
+++ Notes/string_array/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char* fruits[] = {"2 oranges", "2 apples", "3 banans"};
+
+    printf("1st string = %s\n", fruits[0]);
+    printf("2nd string = %s\n", fruits[1]);
+    printf("3rd string = %s\n", fruits[2]);
+
+    return 0;
+}

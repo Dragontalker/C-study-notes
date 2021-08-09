df94aad4b129c3979eb4b076d708a1efc4dc66cc
Richard <richard.yang.tong@gmail.com>
2021-08-09 15:03:32 -0400

    Added notes.

--- Notes/calloc/main.c
+++ Notes/calloc/main.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 2
+
 int main(void)
 {
-    int* ptr = (int*) malloc(sizeof(int));
+    int* ptr = (int*) malloc(N * sizeof(int));
 
-    printf("The address of ptr = %p\n", ptr);
+    if (ptr == NULL) {
+        printf("Memory not available!");
+        exit(1);
+    }
 
-    ptr = (int*) realloc(ptr, 2 * sizeof(int));
+    printf("Enter the two numbers: \n");
+    for (size_t i = 0; i < N; i++) {
+        scanf("%d", ptr + i);
+    }
 
-    printf("The address of ptr = %p\n", ptr);
+    //Memory allocation for 2 more integers
+    ptr = (int*) realloc(ptr, 2 * N * sizeof(int));
+
+    if (ptr == NULL) {
+        printf("Memory not available!");
+        exit(1);
+    }
+
+    printf("Enter the two more numbers: \n");
+    for (size_t i = 2; i < 2 * N; i++) {
+        scanf("%d", ptr + i);
+    }
+
+    for (size_t i = 0; i < 4; i++) {
+        printf(" %d", *(ptr + i));
+    }
 
     return EXIT_SUCCESS;
 }

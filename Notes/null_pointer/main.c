a0f9bfcc2f6dd69cafa6739cedf7850b97135b7c
Richard <richard.yang.tong@gmail.com>
2021-08-09 14:06:41 -0400

    Added notes.

--- Notes/null_pointer/main.c
+++ Notes/null_pointer/main.c
@@ -5,6 +5,9 @@ int main(void) {
 
     int* ptr;
 
+    printf("The initial address = %p\n", ptr);
+    printf("The size of NULL = %d\n", sizeof(NULL));
+
     ptr = (int*) malloc(2 * sizeof(int));
 
     if (ptr == NULL){

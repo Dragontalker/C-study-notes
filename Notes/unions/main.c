6b3da8c38a2e86145a9a9290bf1b5b462acbccd9
Richard <richard.yang.tong@gmail.com>
2021-08-09 08:33:05 -0400

    Added notes.

--- Notes/unions/main.c
+++ Notes/unions/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+union abc {
+    int a;
+    char b;
+} var;
+
 int main()
 {
-    printf("Hello world!\n");
+    var.a = 65;
+    printf("a = %d\n", var.a);
+    printf("b = %c\n", var.b);
+
+    var.b = 'B';
+    printf("a = %d\n", var.a);
+    printf("b = %c\n", var.b);
     return 0;
 }

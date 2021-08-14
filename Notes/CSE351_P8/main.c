420c0842c5b339346897085a7b76a8da877b1831
Richard <richard.yang.tong@gmail.com>
2021-08-14 11:20:12 -0400

    Added notes.

--- Notes/CSE351_P8/main.c
+++ Notes/CSE351_P8/main.c
@@ -2,20 +2,28 @@
 #include <stdlib.h>
 
 void show_bytes(char*, int);
+void show_int(int);
 
 int main(void)
 {
-    char test[] = "Hello world!";
-    show_bytes(test, 14);
+    int a = 12345;
+
+    printf("int a = 12345;\n");
+    show_int(a);
 
     return EXIT_SUCCESS;
 }
 
 void show_bytes(char* start, int len)
 {
-    for (int i = 0; i < len; i++)
+    for (size_t i = 0; i < len; i++)
     {
         printf("%p\t0x%.2x\n", start + i, *(start + i));
     }
     printf("\n");
+}
+
+void show_int(int x)
+{
+    show_bytes( (char*) &x, sizeof(int));
 }

c8c0334dc80f9123513a4748f81d853234ce4506
Richard <richard.yang.tong@gmail.com>
2021-08-14 11:15:26 -0400

    Added notes.

--- Notes/CSE351_P8/main.c
+++ Notes/CSE351_P8/main.c
@@ -6,18 +6,16 @@ void show_bytes(char*, int);
 int main(void)
 {
     char test[] = "Hello world!";
-
     show_bytes(test, 14);
 
     return EXIT_SUCCESS;
 }
 
-void show_bytes(char* start, int len) {
-
-    for (int i = 0; i < len; i++) {
-
+void show_bytes(char* start, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
         printf("%p\t0x%.2x\n", start + i, *(start + i));
     }
-
     printf("\n");
 }

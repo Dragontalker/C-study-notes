3d3368b73bfd7588de7d86098470cee46e541d49
Richard <richard.yang.tong@gmail.com>
2021-08-21 15:41:14 -0400

    Added notes.

--- Notes/process/main.c
+++ Notes/process/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        printf("Hello from child\n");
+    } else {
+        printf("Hello from parent\n");
+    }
+
     return 0;
 }

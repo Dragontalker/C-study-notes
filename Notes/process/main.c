2f2a5ae9948b5e576507029f31209ef72432fc7d
Richard <richard.yang.tong@gmail.com>
2021-08-21 17:39:52 -0400

    Added notes.

--- Notes/process/main.c
+++ Notes/process/main.c
@@ -1,15 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void foo(int, int);
+
 int main()
 {
-    pid_t pid = fork();
-
-    if (pid == 0) {
-        printf("Hello from child\n");
-    } else {
-        printf("Hello from parent\n");
-    }
+    foo(10, 10);
 
     return 0;
+}
+
+void foo(int n, int m)
+{
+    int* p;
+
+    /* allocate a block of n ints */
+    p = (int*) malloc(n * sizeof(int));
+
+    if(p == NULL)
+    {
+        perror("malloc");
+        exit(0);
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        p[i] = i;
+    }
+
+    /* add space for m ints to end of p block */
+    p = (int*) realloc(p, (n + m) * sizeof(int));
+
+    if (p == NULL)
+    {
+        perror("realloc");
+        exit(0);
+    }
+
+    for (size_t i = n; i < n+m; i++)
+    {
+        p[i] = i;
+    }
+
+    /* print new array */
+    for (size_t i = 0; i < n + m; i++)
+    {
+        printf("%d\n", p[i]);
+    }
+
+    free(p);
 }

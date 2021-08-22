cedc6443be9346c129e4bec4982238de2add1987
Richard <richard.yang.tong@gmail.com>
2021-08-22 12:49:15 -0400

    Added notes.

--- C Modren Approach/ex027_pointer/main.c
+++ C Modren Approach/ex027_pointer/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void double_swap(double*, double*);
+
 int main(void)
 {
+    double a = 5.4;
+    double b = 3.8;
+
+    printf("Before swap, a = %.1f, b = %.1f\n", a, b);
+
+    double_swap(&a, &b);
+
+    printf("After swap, a = %.1f, b = %.1f\n", a, b);
 
     return EXIT_SUCCESS;
+}
+
+void double_swap(double* p0, double* p1)
+{
+    double tmp = *(p0);
+    *(p0) = *(p1);
+    *(p1) = tmp;
 }

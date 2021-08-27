dec5881d58663dd35780d889c6967bdf55ea1a58
Richard <richard.yang.tong@gmail.com>
2021-08-27 17:36:44 -0400

    Added notes.

--- Modern C/ex0005_string/main.c
+++ Modern C/ex0005_string/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void) {
+int main(int argc, char* argv[argc + 1]) {
+
+    size_t const len = strlen(argv[0]);
+    char name[len + 1];
+
+    memcpy(name, argv[0], len);
+    name[len] = 0;
+
+    if (!strcmp(name, argv[0])) {
+        printf("program name \"%s\" successfully copied\n", name);
+    } else {
+        printf("copying %s leads to different string %s\n", argv[0], name);
+    }
 
     return EXIT_SUCCESS;
 }

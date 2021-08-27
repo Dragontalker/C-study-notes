5411bb3624c420854bc7a3a2e5f26b140224fd07
Richard <richard.yang.tong@gmail.com>
2021-08-27 18:00:36 -0400

    Added notes.

--- Modern C/ex0007_time_object/main.c
+++ Modern C/ex0007_time_object/main.c
@@ -18,16 +18,20 @@ Time time_set_yday(Time);
 int main(void)
 {
     Time today = {
-        .tm_year = 2019-1900,
-        .tm_mon = 4-1,
-        .tm_mday = 3,
-        .tm_hour = 10,
-        .tm_min = 0,
-        .tm_sec = 47
+        .tm_year = 2021-1900,
+        .tm_mon = 8-1,
+        .tm_mday = 27,
+        .tm_hour = 5,
+        .tm_min = 59,
+        .tm_sec = 11
     };
 
     printf("This year is %d\n", today.tm_year + 1900);
     printf("Next year will be %d\n", today.tm_year + 1900 + 1);
+
+    today = time_set_yday(today);
+
+    printf("Day of the year is %d\n", today.tm_yday);
 
     return EXIT_SUCCESS;
 }
@@ -39,7 +43,7 @@ bool leap_year(unsigned year) {
 
 Time time_set_yday(Time t) {
 
-    t.tm_mday += DAYS_BEFORE[t.tm_mon] + t.tm_mday - 1;
+    t.tm_yday += DAYS_BEFORE[t.tm_mon] + t.tm_mday - 1;
 
     if ((t.tm_mon > 1) && leap_year(t.tm_year + 1900)) {
         t.tm_yday++;

41c8b1e3cd6a3b4da6566ccd7d96a2d1ed73d261
Richard <richard.yang.tong@gmail.com>
2021-08-27 17:09:01 -0400

    Added notes.

--- Modern C/ex0003_enum/main.c
+++ Modern C/ex0003_enum/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef const char* String;
+
+enum corvid { magpie, raven, jay, corvid_num };
+
 int main(void)
 {
+    String const bird[corvid_num] = {
+        [raven] = "raven",
+        [magpie] = "magpie",
+        [jay] = "jay"
+    };
+
+    for (unsigned i = 0; i < corvid_num; i++) {
+        printf("Corvid #%u is the %s\n", i+1, bird[i]);
+    }
 
     return EXIT_SUCCESS;
 }

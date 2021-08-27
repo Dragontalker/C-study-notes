9ea56b608ddaa11846b54af63accf6a303deaf11
Richard <richard.yang.tong@gmail.com>
2021-08-27 15:41:35 -0400

    Added notes.

---
+++ Modern C/ex0001_modern_style/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 5
+
+int main(void) {
+
+    double A[N] = {
+        [0] = 9.0,
+        [1] = 2.9,
+        [4] = 3.E+25,
+        [3] = .00007,
+    };
+
+    for (size_t i = 0; i < N; i++) {
+        printf("element %zu is %g, \tits square is %g\n",
+               i,
+               A[i],
+               A[i]*A[i]);
+    }
+
+    return EXIT_SUCCESS;
+}

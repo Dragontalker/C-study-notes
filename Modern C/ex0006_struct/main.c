1b7f6514c71605b39532a79cd7c8f2b29c9b6367
Richard <richard.yang.tong@gmail.com>
2021-08-27 17:43:14 -0400

    Added notes.

--- Modern C/ex0006_struct/main.c
+++ Modern C/ex0006_struct/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef const char* String;
+
+struct birdStruct {
+    String jay;
+    String magpie;
+    String raven;
+    String chough;
+};
+
+typedef struct birdStruct Bird;
+
 int main(void)
 {
+    Bird const aName = {
+        .chough = "Henry",
+        .raven = "Lissy",
+        .magpie = "Frau",
+        .jay = "Joe"
+    };
 
     return EXIT_SUCCESS;
 }

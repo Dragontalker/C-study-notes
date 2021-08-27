6878b850cffe52958ed3a7d6facdff0028391f17
Richard <richard.yang.tong@gmail.com>
2021-08-27 17:51:45 -0400

    Added notes.

--- Modern C/ex0007_time_object/main.c
+++ Modern C/ex0007_time_object/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define DAYS_BEFORE                             \
+(int const[12]) {                               \
+    [0] = 0, [1] = 31, [2] = 59, [3] = 90,      \
+    [4] = 120, [5] = 151, [6] = 181, [7] = 212, \
+    [8] = 243, [9] = 273, [10] = 304, [11] = 334\
+}
+
+typedef struct tm Time;
 
 int main(void)
 {

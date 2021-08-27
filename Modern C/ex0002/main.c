54844547b83d8ef34357b818a7f0057933b3ed50
Richard <richard.yang.tong@gmail.com>
2021-08-27 16:25:09 -0400

    Added notes.

--- Modern C/ex0002/main.c
+++ Modern C/ex0002/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+#define N 3
+
+int main(void) {
+
+    const char* const bird[N] = {"raven","magpie","jay"};
+    const char* const pronoun[N] = {"we", "you", "they"};
+    const char* const ordinal[N] = {"first", "second", "third"};
+
+    for (unsigned i = 0; i < N; i++) {
+        printf("Corvid %u is the %s\n", i, bird[i]);
+    }
+
+    for (unsigned i = 0; i < N; i++) {
+        printf("%s plural pronoun is %s\n", ordinal[i], pronoun[i]);
+    }
 
     return EXIT_SUCCESS;
 }

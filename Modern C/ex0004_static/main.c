a0a6d07df93eebb401538c8fccea3e6c4d57c2f0
Richard <richard.yang.tong@gmail.com>
2021-08-27 17:30:14 -0400

    Added notes.

--- Modern C/ex0004_static/main.c
+++ Modern C/ex0004_static/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap_double(double [static 2]);
+
 int main(void) {
 
+    double A[2] = { 1.0, 2.0 };
+
+    printf("Before swapping, \n");
+    printf("A[0] = %g\n", A[0]);
+    printf("A[1] = %g\n", A[1]);
+
+    swap_double(A);
+
+    printf("After swapping, \n");
+    printf("A[0] = %g\n", A[0]);
+    printf("A[1] = %g\n", A[1]);
 
     return EXIT_SUCCESS;
+}
+
+void swap_double(double a[static 2]) {
+
+    double temp = a[0];
+    a[0] = a[1];
+    a[1] = temp;
 }

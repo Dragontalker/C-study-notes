57c78d6f0b96e3013938563c13a7abd2bd3bc95c
Richard <richard.yang.tong@gmail.com>
2021-08-28 16:38:12 -0400

    Added notes.

--- Modern C/ex0014_flush/main.c
+++ Modern C/ex0014_flush/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+void delay(double);
+
+int main(int argc, char* argv[argc + 1]) {
+    fputs("waiting 10 seconds for you to stop me", stdout);
+
+    if (argc < 3) {
+        fflush(stdout);
+    }
+
+    for (unsigned i = 0; i < 10; i++) {
+
+        fputs('.', stdout);
+
+        if (argc < 2) {
+            fflush(stdout);
+        }
+
+        delay(1.0);
+    }
+
+    fputs("\n", stdout);
+    fputs("You did ignore me, so bye bye\n", stdout);
 
     return EXIT_SUCCESS;
+}
+
+void delay(double secs) {
+
+    const double magic = 4E8;
+    const unsigned long long nano = secs * magic;
+
+    for (unsigned long volatile count = 0; count < nano; count++) {
+
+    }
 }

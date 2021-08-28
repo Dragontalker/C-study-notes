19193b18804fa52ce1e5a2e21f9818126c7053e5
Richard <richard.yang.tong@gmail.com>
2021-08-28 16:29:22 -0400

    Added notes.

--- Modern C/ex0013_file/main.c
+++ Modern C/ex0013_file/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+int main(int argc, char* argv[argc + 1]) {
+
+    FILE* logFile = fopen("mylog.txt", "a");
+
+    if (!logFile) {
+        perror("fopen failed");
+        return EXIT_FAILURE;
+    }
+
+    fputs("feeling fine today\n", logFile);
 
     return EXIT_SUCCESS;
 }

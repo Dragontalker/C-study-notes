b822cb5daf509eb6dafc1d8469370f7d0593d186
Richard <richard.yang.tong@gmail.com>
2021-08-28 16:19:55 -0400

    Added notes.

--- Modern C/ex0012_putchar/main.c
+++ Modern C/ex0012_putchar/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int puts_manually(char const s[static 1]);
+
 int main(void)
 {
+    puts_manually("Hello world!");
 
     return EXIT_SUCCESS;
+}
+
+int puts_manually(char const s[static 1]) {
+
+    for (size_t i = 0; s[i]; i++) {
+        if (putchar(s[i]) == EOF) {
+            return EOF;
+        }
+    }
+
+    if (putchar('\n') == EOF) {
+        return EOF;
+    }
+
+    return 0;
 }

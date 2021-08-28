99438f338d870f5dc6070d61edad5b39fbba85a7
Richard <richard.yang.tong@gmail.com>
2021-08-28 17:19:15 -0400

    Added notes.

--- Modern C/ex0016_comparator/main.c
+++ Modern C/ex0016_comparator/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int compare_unsigned(const void*, const void*);
+
 int main(void)
 {
 
     return EXIT_SUCCESS;
+}
+
+int compare_unsigned(const void* a, const void* b) {
+
+    const unsigned* A = a;
+    const unsigned* B = b;
+
+    if (*(A) < *(B)) {
+        return -1;
+    } else if (*(A) > *(B)) {
+        return +1;
+    } else {
+        return 0;
+    }
 }

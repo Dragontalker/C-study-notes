0909debb9d79bea69fad0d451a15aa7be9ad1580
Richard <richard.yang.tong@gmail.com>
2021-08-27 21:30:40 -0400

    Added notes.

--- Modern C/ex0011_buffer_fibnacci/main.c
+++ Modern C/ex0011_buffer_fibnacci/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void fib2rec(size_t, size_t[]);
+size_t fib2(size_t);
+
 int main(void)
 {
+    printf("The Fibonacci nuber of 100 = %zu\n", fib2(100));
 
     return EXIT_SUCCESS;
+}
+
+void fib2rec(size_t n, size_t buf[2]) {
+
+    if (n > 2) {
+        size_t res = buf[0] + buf[1];
+        buf[1] = buf[0];
+        buf[0] = res;
+        fib2rec(n-1, buf);
+    }
+}
+
+size_t fib2(size_t n) {
+
+    size_t res[2] = { 1, 1 };
+
+    fib2rec(n, res);
+
+    return res[0];
 }

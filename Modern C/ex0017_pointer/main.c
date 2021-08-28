7bbc954097e425baf8b6c6c3a58bdd52c8efb45c
Richard <richard.yang.tong@gmail.com>
2021-08-28 17:31:15 -0400

    Added notes.

--- Modern C/ex0017_pointer/main.c
+++ Modern C/ex0017_pointer/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double blub(const double*, double*);
+
 int main(void)
 {
+    double c = 35;
+    double d = 3.6;
+
+    printf("blub is %g\n", blub(&c, &d));
+    printf("after blub the sum is %g\n", c + d);
 
     return EXIT_SUCCESS;
+}
+
+double blub(const double* a, double* b) {
+
+    double myA = *(a);
+
+    *(b) = 2 * myA;
+
+    return *(a);
 }

6ac311f5b0b35f49e976c18dd8a6a4ceb413e26b
Richard <richard.yang.tong@gmail.com>
2021-08-28 17:02:00 -0400

    Added notes.

--- Modern C/ex0015_error_exit/main.c
+++ Modern C/ex0015_error_exit/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sayGoodBye(void);
+
 int main(void)
 {
+    atexit(sayGoodBye);
 
     return EXIT_SUCCESS;
+}
+
+void sayGoodBye(void) {
+
+    if (errno) {
+        perror("terminating with error condition...");
+        fputs("Good Bye\n", stderr);
+    }
 }

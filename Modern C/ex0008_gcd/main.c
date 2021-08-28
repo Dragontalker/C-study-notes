9b23c3d80a9b4697a66db6e155d421030d6e99f9
Richard <richard.yang.tong@gmail.com>
2021-08-27 21:01:30 -0400

    Added notes.

--- Modern C/ex0008_gcd/main.c
+++ Modern C/ex0008_gcd/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+size_t gcd(size_t, size_t);
 
 int main(void) {
 
+    printf("The gcd of 15 and 25 = %zu\n", gcd(15, 25));
+
     return EXIT_SUCCESS;
+}
+
+size_t gcd(size_t a, size_t b) {
+
+    assert(a <= b);
+
+    if (!a) {
+        return b;
+    }
+
+    size_t rem = b % a;
+
+    return gcd(rem, a);
 }

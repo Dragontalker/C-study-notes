2fc37a8193049a5df21d0630201f72e6fb6b5522
Richard <richard.yang.tong@gmail.com>
2021-08-27 21:13:07 -0400

    Added notes.

--- Modern C/ex0009_fibnacci/main.c
+++ Modern C/ex0009_fibnacci/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+size_t fib(size_t);
+
 int main(void)
 {
+    printf("The Fibonacci number of 10  = %u\n", fib(10));
 
     return EXIT_SUCCESS;
+}
+
+size_t fib(size_t n) {
+
+    if (n < 3) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
 }

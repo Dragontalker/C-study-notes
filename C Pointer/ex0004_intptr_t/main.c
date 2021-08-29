3ff6c4c1618f01a16dcce617f3b87e0043edbd29
Richard <richard.yang.tong@gmail.com>
2021-08-29 13:11:12 -0400

    Added notes.

--- C Pointer/ex0004_intptr_t/main.c
+++ C Pointer/ex0004_intptr_t/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(void)
 {
+    int num;
+    intptr_t* pi = &num;
+    printf("The address of pi = %p\n", pi);
+
+    unsigned unum;
+    uintptr_t* pu = &unum;
+    printf("The address of pu = %p\n", pu);
 
     return EXIT_SUCCESS;
 }

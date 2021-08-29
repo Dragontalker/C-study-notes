1217f1354ef3aa5562e18bcf2464eda3019abf62
Richard <richard.yang.tong@gmail.com>
2021-08-29 13:37:16 -0400

    Added notes.

--- C Pointer/ex0007_double_ptr/main.c
+++ C Pointer/ex0007_double_ptr/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef const char* String;
+
 int main(void) {
 
-    char* titles[] = {
+    String titles[] = {
         "A tale of Two Cities",
         "Wuthering Heights",
         "Don Quixote",
@@ -13,19 +15,22 @@ int main(void) {
         "Gulliver's Travels"
     };
 
-    char** bestBooks[3];
+    String* bestBooks[3];
     bestBooks[0] = &titles[0];
     bestBooks[1] = &titles[3];
     bestBooks[2] = &titles[5];
 
-    char** englishBooks[4];
+    String* englishBooks[4];
     englishBooks[0] = &titles[0];
     englishBooks[1] = &titles[1];
     englishBooks[2] = &titles[5];
     englishBooks[3] = &titles[6];
 
     printf("The value of bestBook[0] = %p\n", bestBooks[0]);
+    printf("The string value at bestBook[0] = %s\n", *(bestBooks[0]));
+
     printf("The value of englishBook[0] = %p\n", englishBooks[0]);
+    printf("The string value at englishBook[0] = %s\n", *(englishBooks[0]));
 
     return EXIT_SUCCESS;
 }

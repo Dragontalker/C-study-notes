49b32f9d0415e73c47a9915e16e768d64a6d1858
Richard <richard.yang.tong@gmail.com>
2021-08-29 14:42:41 -0400

    Added notes.

--- C Pointer/ex0008_malloc/main.c
+++ C Pointer/ex0008_malloc/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+int main(void) {
+
+    int* pi = (int*) malloc(sizeof(int));
+
+    *(pi) = 5;
+
+    printf("*pi = %d\n", *(pi));
 
     return EXIT_SUCCESS;
 }

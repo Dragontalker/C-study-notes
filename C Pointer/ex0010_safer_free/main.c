b18c342aac35469340fc43184569d7672fe5ae96
Richard <richard.yang.tong@gmail.com>
2021-08-29 16:08:37 -0400

    Added notes.

--- C Pointer/ex0010_safer_free/main.c
+++ C Pointer/ex0010_safer_free/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+void saferFree(void**);
+
+#define safeFree(p) saferFree((void**)&(p))
+
+int main(void) {
+
+    int* pi;
+    pi = (int*) malloc(sizeof(int));
+    *(pi) = 5;
+
+    printf("Before: %p\n", pi);
+
+    safeFree(pi);
+
+    printf("After: %p\n", pi);
+
+    safeFree(pi);
 
     return EXIT_SUCCESS;
+}
+
+void saferFree(void** pp) {
+
+    if (pp != NULL && *(pp) != NULL) {
+        free(*(pp));
+        *(pp) = NULL;
+    }
 }

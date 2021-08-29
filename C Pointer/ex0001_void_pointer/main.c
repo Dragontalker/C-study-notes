bdc0c6a761bc23bd427c8c263ab7777544d71c52
Richard <richard.yang.tong@gmail.com>
2021-08-29 09:41:59 -0400

    Added notes.

--- C Pointer/ex0001_void_pointer/main.c
+++ C Pointer/ex0001_void_pointer/main.c
@@ -3,6 +3,13 @@
 
 int main(void)
 {
+    int num;
+    int* pi = &num;
+    printf("Value of pi = %p\n", pi);
+
+    void* pv = pi;
+    pi = (int*) pv;
+    printf("value of pi = %p\n", pi);
 
     return EXIT_SUCCESS;
 }

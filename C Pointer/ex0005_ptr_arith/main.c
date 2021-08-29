fe49e8e97fe346bdaf3ba495dac560f8164c2aeb
Richard <richard.yang.tong@gmail.com>
2021-08-29 13:15:42 -0400

    Added notes.

--- C Pointer/ex0005_ptr_arith/main.c
+++ C Pointer/ex0005_ptr_arith/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+int main(void) {
+
+    int vector[] = { 28, 41, 7 };
+
+    int* pi = vector;
+    printf("The value of point pi = %p\n", pi);
+    printf("The int value pointed by pi = %d\n", *(pi));
+
+    pi += 1;
+    printf("The value of point pi = %p\n", pi);
+    printf("The int value pointed by pi = %d\n", *(pi));
+
+    pi += 1;
+    printf("The value of point pi = %p\n", pi);
+    printf("The int value pointed by pi = %d\n", *(pi));
 
     return EXIT_SUCCESS;
 }

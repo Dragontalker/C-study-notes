33db1154cc4c7a404bbfab2d324ccef812cbb1aa
Richard <richard.yang.tong@gmail.com>
2021-08-29 10:10:36 -0400

    Added notes.

--- C Pointer/ex0002_pointer_initialization/main.c
+++ C Pointer/ex0002_pointer_initialization/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int* global_pi;
+
+void foo() {
+    static int* static_pi;
+
+    if (static_pi == NULL) {
+        printf("The value of static_pi = NULL\n");
+    } else {
+        printf("The value of static_pi != NULL\n");
+    }
+}
+
 int main(void)
 {
+    if (global_pi == NULL) {
+        printf("The value of global_pi = NULL\n");
+    } else {
+        printf("The value of global_pi != NULL\n");
+    }
+
+    foo();
 
     return EXIT_SUCCESS;
 }

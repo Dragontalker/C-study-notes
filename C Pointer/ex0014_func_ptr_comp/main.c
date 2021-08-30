a223035c01043b9dc414851d4cfd9191b0ca43d1
Richard <richard.yang.tong@gmail.com>
2021-08-30 12:31:07 -0400

    Added notes.

--- C Pointer/ex0014_func_ptr_comp/main.c
+++ C Pointer/ex0014_func_ptr_comp/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+typedef int (*fptrOperation)(int);
+
+int add(int, int);
+int substract(int, int);
+
+int main(void) {
+
+    fptrOperation fptr1 = add;
+
+    if (fptr1 == add) {
+        printf("fptr1 points to add function\n");
+    } else {
+        printf("fptr1 does not point to add function\n");
+    }
 
     return EXIT_SUCCESS;
+}
+
+int add(int num1, int num2) {
+    return num1 + num2;
+}
+
+int subtract(int num1, int num2) {
+    return num1 - num2;
 }

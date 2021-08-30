75435d21e73a1929cf57e67ba93cdf8084a8746d
Richard <richard.yang.tong@gmail.com>
2021-08-30 12:26:46 -0400

    Added notes.

--- C Pointer/ex0013_func_ptr_array/main.c
+++ C Pointer/ex0013_func_ptr_array/main.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef int (*Operation)(int, int);
+typedef int (*fptrOperation)(int, int);
+
+int add(int, int);
+int subtract(int, int);
+void initializeOperationsArray(fptrOperation*);
+int evaluateArray(char, int, int, fptrOperation*);
 
 int main(void)
 {
-    Operation operations[128] = { NULL };
+    fptrOperation operations[128] = { NULL };
+    initializeOperationsArray(operations);
+
+    printf("%d\n", evaluateArray('+', 5, 6, operations));
+    printf("%d\n", evaluateArray('-', 5, 6, operations));
 
     return EXIT_SUCCESS;
+}
+
+void initializeOperationsArray(fptrOperation* operations) {
+    operations['+'] = add;
+    operations['-'] = subtract;
+}
+
+int evaluateArray(char opcode, int num1, int num2, fptrOperation* operations) {
+    fptrOperation operation;
+    operation = operations[opcode];
+    return operation(num1, num2);
 }

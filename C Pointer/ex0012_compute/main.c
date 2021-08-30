4624daedb97ff7f95d23138b045fcbe4c5a7e404
Richard <richard.yang.tong@gmail.com>
2021-08-30 12:09:14 -0400

    Added notes.

--- C Pointer/ex0012_compute/main.c
+++ C Pointer/ex0012_compute/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int add(int, int);
+int subtract(int, int);
+
+typedef int (*fptrOperation)(int, int);
+int compute(fptrOperation, int, int);
+
 int main(void)
 {
+    printf("%d\n", compute(add, 5, 6));
+    printf("%d\n", compute(subtract, 5, 6));
 
     return EXIT_SUCCESS;
+}
+
+int add(int num1, int num2) {
+    return num1 + num2;
+}
+
+int subtract(int num1, int num2) {
+    return num1 - num2;
+}
+
+int compute(fptrOperation operation, int num1, int num2) {
+    return operation(num1, num2);
 }

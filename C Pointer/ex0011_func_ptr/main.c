67001349e99e78962a87ebe36bd014c899e329e7
Richard <richard.yang.tong@gmail.com>
2021-08-30 11:56:04 -0400

    Added notes.

--- C Pointer/ex0011_func_ptr/main.c
+++ C Pointer/ex0011_func_ptr/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int square(int);
+
+int (*fptr1)(int);
+
 int main(void)
 {
+    int n = 5;
+    fptr1 = square;
+
+    printf("%d squared is %d\n", n, fptr1(n));
 
     return EXIT_SUCCESS;
+}
+
+int square(int num) {
+    return num * num;
 }

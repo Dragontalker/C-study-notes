2c0b31a5e080ff2e66c4a6162a16652e648aa358
Richard <richard.yang.tong@gmail.com>
2021-08-30 12:36:48 -0400

    Added notes.

--- C Pointer/ex0015_different_func_ptr/main.c
+++ C Pointer/ex0015_different_func_ptr/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int (*fptrToSingleInt)(int);
+typedef int (*fptrToTwoInts)(int, int);
+
+int add(int, int);
+
 int main(void) {
 
+    fptrToTwoInts fptrFirst = add;
+
+    fptrToSingleInt fptrSecond = (fptrToSingleInt) fptrFirst;
+    fptrFirst = (fptrToTwoInts) fptrSecond;
+
+    printf("%d\n", fptrFirst(5, 6));
+
     return EXIT_SUCCESS;
+}
+
+int add(int num1, int num2) {
+    return num1 + num2;
 }

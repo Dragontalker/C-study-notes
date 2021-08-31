5ad1e606e156100f11db38048009c7c8a54151b6
Richard <richard.yang.tong@gmail.com>
2021-08-31 11:42:23 -0400

    Added notes.

--- C Pointer/ex0016_void_func_ptr/main.c
+++ C Pointer/ex0016_void_func_ptr/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef void (*fptrBase)();
+typedef int (*fptrToSingleInt)(int);
+typedef int (*fptrToTwoInts)(int, int);
+
+int add(int, int);
+
 int main(void) {
 
+    fptrToTwoInts fptrFirst = add;
+
+    fptrBase basePointer;
+    basePointer = (fptrToSingleInt)fptrFirst;
+
+    fptrFirst = (fptrToTwoInts)basePointer;
+
+    printf("%d\n", fptrFirst(5, 6));
+
     return EXIT_SUCCESS;
+}
+
+int add(int num1, int num2) {
+    return num1 + num2;
 }

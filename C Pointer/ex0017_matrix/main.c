b2e61760e1d3c09b028a22a7ae7b9427cb2eafe4
Richard <richard.yang.tong@gmail.com>
2021-09-01 10:53:11 -0400

    Added notes.

--- C Pointer/ex0017_matrix/main.c
+++ C Pointer/ex0017_matrix/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define M 2
+#define N 3
+
 int main(void)
 {
+    int matrix[M][N] = { { 1, 2, 3}, { 4, 5, 6 } };
+
+    for (unsigned i = 0; i < M; i++)
+    {
+        printf(
+           "&matrix[%d]: %p, sizeof(matrix[%d]): %d\n",
+           i,
+           &matrix[i],
+           i,
+           sizeof(matrix[i])
+        );
+    }
 
     return EXIT_SUCCESS;
 }

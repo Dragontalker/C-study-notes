61fa7401b0833f408ad572e3b99ebe8bc92e0e7b
Richard <richard.yang.tong@gmail.com>
2021-09-02 16:33:24 -0400

    Added notes.

--- C Pointer/ex0018_arr_ptr/main.c
+++ C Pointer/ex0018_arr_ptr/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 5
 int main(void)
 {
+    int vector[N] = { 12, 34, 56, 78, 100 };
+
+    int* pv = (int*) malloc(5 * sizeof(int));
+
+    for (int i = 0; i < N; i++) {
+        *(pv + i) = vector[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        printf("#%d element in pv = %d\n", i, *(pv + i));
+    }
+
+    free(pv);
 
     return EXIT_SUCCESS;
 }

ba437a15b7262c421cfca2f25334fe0e022d4f4d
Richard <richard.yang.tong@gmail.com>
2021-09-03 11:20:58 -0400

    Added notes.

--- C Pointer/ex0019_size_increment/main.c
+++ C Pointer/ex0019_size_increment/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char* getLine(void);
+
 int main(void)
 {
 
     return EXIT_SUCCESS;
+}
+
+char* getLine(void)
+{
+    const size_t sizeIncrement = 10;
+    char* buffer = malloc(sizeIncrement);
+    char* currentPosition = buffer;
+    size_t maximumLength = sizeIncrement;
+    size_t length = 0;
+    int character;
+    if (currentPosition == NULL)
+    {
+        return NULL;
+    }
+    while (1)
+    {
+        character = fgetc(stdin);
+        if (character == '\n')
+        {
+            break;
+        }
+        if (++length >= maximumLength)
+        {
+            char* newBuffer = realloc(buffer, maximumLength += sizeIncrement);
+            if (newBuffer == NULL) {
+                free(buffer);
+                return NULL;
+            }
+            currentPosition = newBuffer + (currentPosition - buffer);
+        }
+        *(currentPosition)++ = character;
+    }
+    *(currentPosition) = '\0';
+    return buffer;
 }

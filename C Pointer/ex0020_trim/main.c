74d8b2d205b66f09125b4ee5b1d76e90b599306a
Richard <richard.yang.tong@gmail.com>
2021-09-04 15:07:13 -0400

    Added notes.

--- C Pointer/ex0020_trim/main.c
+++ C Pointer/ex0020_trim/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char* trim(char*);
+
 int main(void) {
 
     return EXIT_SUCCESS;
+}
+
+char* trim(char* phrase) {
+    char* old = phrase;
+    char* new = phrase;
+
+    while(*(old) == ' ') {
+        old++;
+    }
+
+    while(*(old)) {
+        *(new++) = *(old++);
+    }
+
+    *(new) = 0;
+
+    return (char*) realloc(phrase, strlen(phrase)+1);
 }

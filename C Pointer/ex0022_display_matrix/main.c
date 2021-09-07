1d6e37c1aeedc3429a337e908b2eed9bd8f5f5bd
Richard <richard.yang.tong@gmail.com>
2021-09-07 11:14:03 -0400

    Added notes.

--- C Pointer/ex0022_display_matrix/main.c
+++ C Pointer/ex0022_display_matrix/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define M 2
+#define N 5
+
+void display2DArray(int[][N], int);
+
 int main(void)
 {
-    int matrix[2][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
+    int matrix[M][N] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
 
-    for(unsigned i = 0; i < 2; i++)
+    for(unsigned i = 0; i < M; i++)
     {
-        for(unsigned j = 0; j < 5; j++)
+        for(unsigned j = 0; j < N; j++)
         {
             printf(
                 "matrix[%d][%d] Address: %p Value: %d\n",
@@ -19,7 +24,7 @@ int main(void)
         }
     }
 
-    int (*pmatrix)[5] = matrix;
+    int (*pmatrix)[N] = matrix;
 
     printf("%p\n", pmatrix);
     printf("%p\n", pmatrix + 1);
@@ -27,5 +32,19 @@ int main(void)
 
     printf("%p %d\n", pmatrix[0] + 1, *(pmatrix[0] + 1));
 
+    display2DArray(matrix, M);
+
     return EXIT_SUCCESS;
+}
+
+void display2DArray(int arr[][N], int rows)
+{
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            printf("[%d] ", arr[i][j]);
+        }
+        printf("\n");
+    }
 }

71fdcbc277a955e31d7303905b10b37e4e6b5b1b
Richard <richard.yang.tong@gmail.com>
2021-09-09 09:28:48 -0400

    Added notes.

--- C Pointer/ex0026_char_length/main.c
+++ C Pointer/ex0026_char_length/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(void) {
-    printf("%u\n", sizeof(char));
-    printf("%u\n", sizeof('a'));
+    printf("%u\n", sizeof(char)); // 1
+    printf("%u\n", sizeof('a')); // 4 bc C treats char as int
     return EXIT_SUCCESS;
 }
